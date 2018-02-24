8196ed66a7a1cf22c8a4270ed61de0c180728735
Stephan-Kashkarov <stephankashkarovofficial@gmail.com>
2018-02-25 08:18:04 +1100

    added css
    made changes to js and linked html better

--- main/js/main.js
+++ main/js/main.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function() {                       // READY FUNCTION
 
 	//BASIC SELECTORS
-	$("body p.lead").css("border", "4px solid red") // CSS STYLE SELECT
-	$("#id").css("border", "4px solid grey") // ID SELECT
-	$("*").css("border", "4px solid blue") // ALL SELECT
+	$("body p.lead").css("border", "4px solid red"); // CSS STYLE SELECT
+	$("#lesson-1").css("border", "4px solid grey");  // ID SELECT
+	$("*").css("text-decoration", "none");           // ALL SELECT
 
 	// BASIC ANIMATIONS
-	$(".box:first").hide
+	$(".box:first").hide(500).delay(300).show(800); // HIDE SHOW DELAY ANIMATIONS
+	$(".box:last").slideUp(500).slideDown(800);     // SLIDE ANIMATION
+	$(".box:nth-child(2)").fadeOut(500).fadeIn(500);      // FADE ANIMATION
+
 	// INDEX FILTERS
 
 	// RELATIONSHIP FILTERS
