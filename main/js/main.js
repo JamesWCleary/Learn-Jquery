a2cd53a929091b44cc58fc06a2eb304d15443158
Stephan-Kashkarov <stephankashkarovofficial@gmail.com>
2018-02-25 07:59:19 +1100

    downloaded a html template
    also advanced my js

--- main/js/main.js
+++ main/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-	//basic selectors
-	$("body p.lead").css("border", "4px solid red")
-	$("#id").css("border", "4px solid red")
+	//BASIC SELECTORS
+	$("body p.lead").css("border", "4px solid red") // CSS STYLE SELECT
+	$("#id").css("border", "4px solid grey") // ID SELECT
+	$("*").css("border", "4px solid blue") // ALL SELECT
+
+	// BASIC ANIMATIONS
+	$(".box:first").hide
+	// INDEX FILTERS
+
+	// RELATIONSHIP FILTERS
+
+	// ATTRIBUTE FILTERS
+
+
 });
